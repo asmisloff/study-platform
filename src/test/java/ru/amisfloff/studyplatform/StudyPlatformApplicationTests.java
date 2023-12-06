0892067ca0ed556525f1097623add252f13a8f4a
asmisloff <a.smisloff@gmail.com>
2023-12-06 21:12:06 +0300

    init

---
+++ src/test/java/ru/amisfloff/studyplatform/StudyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.amisfloff.studyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
